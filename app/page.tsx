eeda88eeee404a4b01324fce885ad0ae6cef5efa
Perawit <perawit8748@gmail.com>
2026-07-27 16:07:48 +0700

    add: form

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,74 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="container p-4 d-flex justify-content-center">
+      <div className="col-lg-8">
+        <div className="card shadow-lg border-0 rounded-4">
+          <div className="card-body p-4">
+            <div className="d-flex justify-content-center">
+              <h2 className="mb-4 ">Patient Registration Form</h2>{" "}
+            </div>
+
+            <div className="row">
+              <div className="col-md-4 mb-3">
+                <label className="form-label">First Name</label>
+                <input
+                  type="text"
+                  className="form-control form-control-sm border border-primary rounded"
+                />
+              </div>
+
+              <div className="col-md-4 mb-3">
+                <label className="form-label">Middle Name</label>
+                <input
+                  type="text"
+                  className="form-control form-control-sm border border-primary rounded"
+                />
+              </div>
+
+              <div className="col-md-4 mb-3">
+                <label className="form-label">Last Name</label>
+                <input
+                  type="text"
+                  className="form-control form-control-sm border border-primary rounded"
+                />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-4 mb-3">
+                <label className="form-label">Date of birth</label>
+                <input
+                  type="date"
+                  className="form-control form-control-smborder border-primary rounded"
+                />
+              </div>
+
+              <div className="col-md-4 mb-3">
+                <label className="form-label">Gender</label>
+                <select
+                  className="form-select border border-primary"
+                  id="gender"
+                  name="gender"
+                >
+                  <option value="">Select Gender</option>
+                  <option value="male">Male</option>
+                  <option value="female">Female</option>
+                  <option value="other">Other</option>
+                </select>
+              </div>
+              <div className="col-md-4 mb-3">
+                <label className="form-label">Phone Number</label>
+                <input
+                  className="form-control border-primary rounded"
+                  type="tel"
+                />
+              </div>
+
+              <div className="row mb-3.5"></div>
+            </div>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
     </div>
   );
 }
